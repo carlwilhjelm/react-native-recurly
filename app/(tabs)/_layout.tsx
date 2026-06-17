52366bd9876d6509b224b8b69df53837a02eb501
Carl Wilhjelm <carlwilhjelm@gmail.com>
2026-06-16 21:40:40 -0400

    moved to NativeTabs and used generic icons for now

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,28 @@
-import { Tabs } from 'expo-router';
+import { NativeTabs, Icon, Label } from 'expo-router/unstable-native-tabs';
 
-const TabLayout = () => (
-  <Tabs screenOptions={{ headerShown: false }}>
-    <Tabs.Screen name="index" options={{ title: 'Home' }}></Tabs.Screen>
-    <Tabs.Screen name="subscriptions" options={{ title: 'Subscriptions' }}></Tabs.Screen>
-    <Tabs.Screen name="insights" options={{ title: 'Insights' }}></Tabs.Screen>
-    <Tabs.Screen name="settings" options={{ title: 'Settings' }}></Tabs.Screen>
-    <Tabs.Screen name="subscriptions/[id]" options={{href: null}}></Tabs.Screen>
-  </Tabs>
-);
-
-export default TabLayout;
+export default function TabLayout() {
+    return (
+      <NativeTabs>
+        <NativeTabs.Trigger name="index">
+          <Label>Home</Label>
+          <Icon sf="house.fill" drawable="custom_android_drawable" />
+        </NativeTabs.Trigger>
+        <NativeTabs.Trigger name="subscriptions">
+          <Icon sf="bag" drawable="custom_settings_drawable" />
+          <Label>Subscriptions</Label>
+        </NativeTabs.Trigger>
+        <NativeTabs.Trigger name="insights">
+          <Icon sf="eye" drawable="custom_settings_drawable" />
+          <Label>Insights</Label>
+        </NativeTabs.Trigger>
+        <NativeTabs.Trigger name="settings">
+          <Icon sf="gear" drawable="custom_settings_drawable" />
+          <Label>Settings</Label>
+        </NativeTabs.Trigger>
+        <NativeTabs.Trigger name="subscriptions/[id].tsx" hidden={true}>
+          <Icon sf="0.circle" drawable="custom_settings_drawable" />
+          <Label>Settings</Label>
+        </NativeTabs.Trigger>
+      </NativeTabs>
+    );
+}
